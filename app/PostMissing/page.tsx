7ca02047635aaf7bb05f1028ddd75027549eb965
MCFan99 <maclex3983@yahoo.com>
2026-09-10 17:15:38 +0000

    Edited Post Missing page to work for everyone

--- app/PostMissing/page.tsx
+++ app/PostMissing/page.tsx
@@ -3,7 +3,7 @@ import style from "./page.module.css";
 import { redirect, useRouter } from "next/navigation";
 
 export default function PostMissing() {
-    const router = useRouter()
+    const router = useRouter();
     return (
         <div className={`${style.page} concert_one_regular`}>
             <div className={style.main}>
@@ -12,8 +12,23 @@ export default function PostMissing() {
                 </h1>
                 <div className={style.postmissing_container}>
                     <div className={style.page_add_image}></div>
-                    <button className={style.page_return} onClick={() => {redirect('https://silver-space-memory-wvp6pvr4rph9pwr-3000.app.github.dev')}}>
-                        <h1>Cancle Post</h1>
+                    <div className={`${style.page_add_name} concert_one_regular`}>
+                        <p>Name:</p>
+                        <input type="name" placeholder={"Greg"} onChange={(e) => {}} />
+                    </div>
+                    <button className={`${style.page_return} concert_one_regular`} onClick={() => {redirect('/')}}>
+                        <p>Cancle Post</p>
+                    </button>
+                    <div className={`${style.page_add_location} concert_one_regular`}>
+                        <h2>Last seen:</h2>
+                        <input id="avatar" type="file" name="avatar" accept="image/png, image/jpeg" />
+                    </div>
+                    <div className={`${style.page_species} concert_one_regular`}>
+                        <p>Species:</p>
+                        <input type="species" placeholder={"Dog"} onChange={(e) => {}} />
+                    </div>
+                    <button className={`${style.page_post} concert_one_regular`} onClick={() => {redirect('/')}}>
+                        <p>Post</p>
                     </button>
                 </div>
             </div>
