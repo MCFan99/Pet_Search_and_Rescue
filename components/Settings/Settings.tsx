94af0b3d21195fc56015931711228181be010cad
Draxellanen <201153899+Draxellanen@users.noreply.github.com>
2026-09-17 17:29:22 +0000

    Color themes, more work on settings page

--- components/Settings/Settings.tsx
+++ components/Settings/Settings.tsx
@@ -1,14 +1,17 @@
 'use client'
 import style from "./Settings.module.css"
-import test from "./globals.css";
+import global from "./globals.css";
 import { redirect } from "next/navigation";
 export function Settings(){
     return (
         <div className={style.settings_container}>
-            <button className={style.settings_light_mode} onClick={() => {}}>
+             <button className={style.settings_default_mode} onClick={() => { document.documentElement.dataset.theme = "default" }}>
+                <h3 className="concert_one_regular">🔵 Default Mode</h3>
+            </button>
+            <button className={style.settings_light_mode} onClick={() => { document.documentElement.dataset.theme = "light" }}>
                 <h3 className="concert_one_regular">⚪ Light Mode</h3>
             </button>
-            <button className={style.settings_dark_mode} onClick={() => {redirect("/PostMissing")}}>
+            <button className={style.settings_dark_mode} onClick={() => { document.documentElement.dataset.theme = "dark" }}>
                 <h3 className="concert_one_regular">⚫ Dark Mode</h3>
             </button>
             <div className={style.settings_posters}>
